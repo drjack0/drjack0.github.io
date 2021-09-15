f3e1cd1d5f8d8f5d8e8cad52b0f15db8e8206631
drjack0 <drjackdevelopment@gmail.com>
2021-09-15 17:43:44 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-aa9601836dc75f0758dd.js"
+    "url": "webpack-runtime-93516588240eccbfef73.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "56d999e659f658786e7966b43b9df63b"
+    "revision": "49df23b716103c218465a24d4a31ae41"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
